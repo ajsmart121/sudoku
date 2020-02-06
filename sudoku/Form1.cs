ffbbe8366f567728ba00889a792c2323b145e932
Craig <craig.ritchie450@gmail.com>
2020-02-06 23:30:22 +0000

    Simple grid generation
    Generates a solution and then adds a desired amount of numbers to the grid. Solution does have a pattern that may be noticed

--- sudoku/Form1.cs
+++ sudoku/Form1.cs
@@ -20,6 +20,7 @@ namespace sudoku
         public frmgame()
         {
             InitializeComponent();
+            FillGrid(GenerateSolution(), 30);
             for (int x = 0; x < btns.GetLength(0); x++)         // Loop for x
             {
                 for (int y = 0; y < btns.GetLength(1); y++)     // Loop for y
@@ -27,9 +28,20 @@ namespace sudoku
                     btns[x, y] = new Button();
                     btns[x, y].SetBounds(55 * x, 55 * y, 45, 45);
                     btns[x, y].BackColor = Color.PowderBlue;
-                    btns[x, y].Text = Convert.ToString((x + 1) + "," + (y + 1));
+                    if(grid[x,y] != 0)
+                    {
+                        btns[x, y].Text = Convert.ToString(grid[x, y]);
+                        btns[x, y].Enabled = false;
+                    }
+                    else
+                    {
+                        btns[x, y].Text = "";
+                    }
+                    
                     btns[x, y].Click += new EventHandler(this.btnsEvent_Click);
                     Controls.Add(btns[x, y]);
+
+                    
 
                 }
             }
@@ -65,6 +77,115 @@ namespace sudoku
                 }
             }
             return count;
+        }
+
+
+        int[,] GenerateSolution()
+        {
+            Random rand = new Random();
+            int[,] tmp = new int[9, 9];
+            int[] tmp2 = new int[9];
+            
+            for (int i = 0; i < 9; i++)
+            {
+                int val = rand.Next(1, 10);
+                while (!checkSide(0, val))
+                {
+                    val = rand.Next(1, 10);
+                }
+                grid[i, 0] = val;
+                tmp2[i] = val;
+            }
+
+            int desRow = rand.Next(0, 3);
+            for (int i = 0; i < 9; i++)
+            {
+                tmp[i, desRow * 3] = grid[i, 0];
+                grid[i, 0] = 0;
+            }
+
+            for(int j = 1; j < 3; j++)
+            {
+                ShiftRow(tmp2, 3);
+                for (int i = 0; i < 9; i++)
+                {
+                    tmp[i, desRow * 3 + j] = tmp2[i];
+                }
+            }
+
+            int tmpRow;
+            do
+            {
+                tmpRow = rand.Next(0, 3);
+            } while (tmpRow == desRow);
+
+            for(int k = 0; k < 2; k++)
+            {
+                ShiftRow(tmp2, 1);
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int i = 0; i < 9; i++)
+                    {
+                        tmp[i, tmpRow * 3 + j] = tmp2[i];
+                    }
+                    if (j < 2)
+                    {
+                        ShiftRow(tmp2, 3);
+                    }
+                }
+                tmpRow = 3 - tmpRow - desRow;
+            }
+            return tmp;
+        }
+
+        void FillGrid(int[,] sol, int amount)
+        {
+            Random rand = new Random();
+            for (int i = 0; i <(amount+4)/2; i++)
+            {
+                int x, y = 0;
+                do
+                {
+                    x = rand.Next(0, 9);
+                    y = rand.Next(0, 9);
+                } while (grid[x, y] != 0);
+                grid[x, y] = sol[x, y];
+                int choice = rand.Next(0, 2);
+                if(choice == 0)
+                {
+                    grid[8 - y, 8 - x] = sol[8 - y, 8 - x];
+                }
+                else
+                {
+                    grid[y, x] = sol[y, x];
+                }  
+            }
+        }
+
+        void ShiftRow(int[] row, int amount)
+        {
+            for(int i = 0; i < amount; i++)
+            {
+                int start = row[0];
+                for(int j = 0; j < row.Length - 1; j++)
+                {
+                    row[j] = row[j + 1];
+                }
+                row[row.Length-1] = start;
+            }
+        }
+
+
+        public bool checkSide(int y, int value)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                if (grid[i, y] == value)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         private void Frmgame_Load(object sender, EventArgs e)
